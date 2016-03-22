4d7ea276a3aa342b5f4289a009725cc00abdb4af
codesniper <568261070@qq.com>
2016-03-22 15:58:17 +0800

    nothing

---
+++ app/build/generated/source/buildConfig/debug/novel_detective/tedu/cn/novel_detective/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package novel_detective.tedu.cn.novel_detective;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "novel_detective.tedu.cn.novel_detective";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
